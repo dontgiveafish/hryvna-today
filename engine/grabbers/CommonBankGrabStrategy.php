89fea35af05a51d3368675a79e3c0bd450297025
abagayev <abagayev@gmail.com>
2016-11-21 23:32:49 +0200

    Added curl error to exception

--- engine/grabbers/CommonBankGrabStrategy.php
+++ engine/grabbers/CommonBankGrabStrategy.php
@@ -65,6 +65,11 @@ class CommonBankGrabStrategy extends ExchangeRateGrabberStrategyAbstract impleme
         curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36');
         $str = curl_exec($curl);
 
+        $error = curl_error($curl);
+        if (!empty($error)) {
+            throw new \LogicException('curl error ' . $error);
+        }
+
         if (empty($str)) {
             throw new \LogicException('broken remote:no document on link');
         }
